c675d65d43b7f9140b28a922d0c66c539ef85817
usha-git-hub <113238878+usha-git-hub@users.noreply.github.com>
2023-01-16 07:57:50 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,7 +1,19 @@
 const todoReducer = (state, action) => {
     switch (action.type) {
+  switch (action.type) {
+    case "add_todo":
+      return [...state, newTodo(action.payload.title)];
+    case "delete_todo":
+      return state.filter((task) => task.id !== action.payload.id);
+    default:
+      return state;
+  }
+};
 
     }
+function newTodo(task) {
+  return { id: Date.now(), title: task };
 }
 
 export { todoReducer }
+export { todoReducer };
