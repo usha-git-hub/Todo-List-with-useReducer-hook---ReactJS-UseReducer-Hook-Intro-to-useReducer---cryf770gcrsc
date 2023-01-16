3692e97ec7db5a8d67636140372bee155d1fee8f
usha-git-hub <113238878+usha-git-hub@users.noreply.github.com>
2023-01-16 07:47:24 +0530

    Update AddTodo.js

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,11 +1,28 @@
-import React from "react"
+import React, {  useState } from "react";
 
 const AddTodo = ({ dispatch }) => {
 
+  const [todo, setTodo] = useState("");
+  return (
+    <>
+      <form
+        id="todo-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({ type: "add_todo", payload: { title: todo } });
+          setTodo("");
+        }}
+      >
+        <input
+          id="todo-input"
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </>
+  );
+};
 
-    return (
-        <></>
-    )
-}
-
-export { AddTodo }
+export { AddTodo };
