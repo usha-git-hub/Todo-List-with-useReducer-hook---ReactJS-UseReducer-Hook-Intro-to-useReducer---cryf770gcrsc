33aafd8421c1e05bb5fedae5b1190b17259acf1b
usha-git-hub <113238878+usha-git-hub@users.noreply.github.com>
2023-01-16 07:48:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
-import React, { useReducer } from 'react'
-import { todoReducer } from '../reducers/todo';
-import '../styles/App.css';
-import { AddTodo } from './AddTodo';
-import { Todo } from './Todo';
+import React, { useReducer } from "react";
+import { todoReducer } from "../reducers/todo";
+import "../styles/App.css";
+import { AddTodo } from "./AddTodo";
+import { Todo } from "./Todo";
 const App = () => {
-  const [state,dispatch] = useReducer(todoReducer,{/* Initial todo state array, could be an array for storing indivial todo objects */})
-  
- 
+  const [state, dispatch] = useReducer(todoReducer, []);
+
   return (
     <div id="main">
-      <AddTodo />
-     {/* Render list of Todo Components here */}
-    </div>
-  )
-}
+      <AddTodo dispatch={dispatch} />
+      {state.map((task) => (
+ 
+        <Todo
+          key={task.id}
+          id={task.id}
+          value={task.title}
+          dispatch={dispatch}
+        />
 
+      ))}
+    </div>
+  );
+};
 
 export default App;
