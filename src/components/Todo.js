a44109b69c8b40f476c39195cef6f9bc3c39b82b
usha-git-hub <113238878+usha-git-hub@users.noreply.github.com>
2023-01-16 07:50:31 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,16 @@
-import React from 'react';
-const Todo = () => {
-    return (
-        <></>
-    )
-}
+import React from "react";
+const Todo = ({ id, value, dispatch }) => {
+  return (
+    <div className="todo">
+      <div className="todo-title">{value}   <button
+        className="todo-delete"
+        onClick={() => dispatch({ type: "delete_todo", payload: { id: id } })}
+      >
+        Delete
+      </button></div>
+   
+    </div>
+  );
+};
 
-
-export { Todo }
-
+export { Todo };
